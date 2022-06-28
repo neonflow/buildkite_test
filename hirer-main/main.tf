238eab4be09fe451086e23c3ff740af88b1af169
neonflow <54106551+neonflow@users.noreply.github.com>
2022-06-28 10:18:30 +1000

    Update main.tf

--- hirer-main/main.tf
+++ hirer-main/main.tf
@@ -1,1 +1,33 @@
+resource "aws_security_group" "allow_tls_buildkite" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-9ee90ce3"
 
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls_buildkite"
+  }
+}
